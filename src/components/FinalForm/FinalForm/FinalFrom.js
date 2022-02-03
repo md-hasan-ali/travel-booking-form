d539106eb8eafc433086242a9b6fa527d09b591f
md-hasan-ali <hasanaliringku@gmail.com>
2022-02-03 15:18:34 +0600

    final commit

--- src/components/FinalForm/FinalForm/FinalFrom.js
+++ src/components/FinalForm/FinalForm/FinalFrom.js
@@ -34,7 +34,6 @@ const FinalFrom = () => {
                         <input onClick={() => setTrip(true)} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
                         <label className="form-check-label" htmlFor="inlineRadio1">One Way</label>
                     </div>
-
                     <div className="form-check form-check-inline">
                         <input onClick={() => setTrip(false)} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
                         <label className="form-check-label" htmlFor="inlineRadio2">Return Trip</label>
